78994e6ad2b62974de6ae7aef582d7e29eb965bb
Tobi <Tobi@DESKTOP-EJSLH8M>
2017-01-02 14:28:21 +0100

    Added saucer class.

---
+++ AsteroidsOpenGL/saucerClass.cpp
@@ -0,0 +1,12 @@
+#include "saucerClass.h"
+
+
+
+saucerClass::saucerClass()
+{
+}
+
+
+saucerClass::~saucerClass()
+{
+}
---
+++ AsteroidsOpenGL/saucerClass.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "gameObject.h"
+class saucerClass :
+	public gameObject
+{
+private:
+
+public:
+	saucerClass();
+	~saucerClass();
+};
+
