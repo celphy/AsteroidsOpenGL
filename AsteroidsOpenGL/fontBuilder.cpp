4e06829c05e18f0d6cce7afbf67865f6c1bd6c6b
celphy <tobiascarstengoetz@gmail.com>
2017-02-15 23:17:59 +0100

    Added segment logic for fontBuilder and created code to take advantage of that. Added numbers 0-5 for now.

--- AsteroidsOpenGL/fontBuilder.cpp
+++ AsteroidsOpenGL/fontBuilder.cpp
@@ -2,8 +2,108 @@
 
 
 
-gameObject * fontBuilder::makeToText(Point p, float size)
+gameObject * fontBuilder::makeToText(int number, Point p, float size)
 {
 	gameObject* obj = new gameObject(p);
+	obj->setPosition(p);
+	switch (number) {
+	case 0: {
+		GLfloat points[10];
+		points[0] = this->segments[0].x * size;
+		points[1] = this->segments[0].y * size;
+		points[2] = this->segments[1].x * size;
+		points[3] = this->segments[1].y * size;
+		points[4] = this->segments[5].x * size;
+		points[5] = this->segments[5].y * size;
+		points[6] = this->segments[4].x * size;
+		points[7] = this->segments[4].y * size;
+		points[8] = this->segments[0].x * size;
+		points[9] = this->segments[0].y * size;
+		obj->setOutline(4, points);
+		break;
+	}
+
+	case 1: {
+		GLfloat points[4];
+		points[0] = this->segments[1].x * size;
+		points[1] = this->segments[1].y * size;
+		points[2] = this->segments[5].x * size;
+		points[3] = this->segments[5].y * size;
+		obj->setOutline(2, points);
+		break;
+	}
+
+	case 2: {
+		GLfloat points[12];
+		points[0] = this->segments[0].x * size;
+		points[1] = this->segments[0].y * size;
+		points[2] = this->segments[1].x * size;
+		points[3] = this->segments[1].y * size;
+		points[4] = this->segments[3].x * size;
+		points[5] = this->segments[3].y * size;
+		points[6] = this->segments[2].x * size;
+		points[7] = this->segments[2].y * size;
+		points[8] = this->segments[4].x * size;
+		points[9] = this->segments[4].y * size;
+		points[10] = this->segments[5].x * size;
+		points[11] = this->segments[5].y * size;
+		obj->setOutline(6, points);
+		break;
+	}
+
+	case 3: {
+		GLfloat points[14];
+		points[0] = this->segments[0].x * size;
+		points[1] = this->segments[0].y * size;
+		points[2] = this->segments[1].x * size;
+		points[3] = this->segments[1].y * size;
+		points[4] = this->segments[3].x * size;
+		points[5] = this->segments[3].y * size;
+		points[6] = this->segments[2].x * size;
+		points[7] = this->segments[2].y * size;
+		points[8] = this->segments[3].x * size;
+		points[9] = this->segments[3].y * size;
+		points[10] = this->segments[5].x * size;
+		points[11] = this->segments[5].y * size;
+		points[12] = this->segments[4].x * size;
+		points[13] = this->segments[4].y * size;
+		obj->setOutline(7, points);
+		break;
+	}
+	case 4: {
+		GLfloat points[10];
+		points[0] = this->segments[0].x * size;
+		points[1] = this->segments[0].y * size;
+		points[2] = this->segments[2].x * size;
+		points[3] = this->segments[2].y * size;
+		points[4] = this->segments[3].x * size;
+		points[5] = this->segments[3].y * size;
+		points[6] = this->segments[1].x * size;
+		points[7] = this->segments[1].y * size;
+		points[8] = this->segments[5].x * size;
+		points[9] = this->segments[5].y * size;
+		obj->setOutline(5, points);
+		break;
+	}
+	case 5: {
+		GLfloat points[12];
+		points[0] = this->segments[1].x * size;
+		points[1] = this->segments[1].y * size;
+		points[2] = this->segments[0].x * size;
+		points[3] = this->segments[0].y * size;
+		points[4] = this->segments[2].x * size;
+		points[5] = this->segments[2].y * size;
+		points[6] = this->segments[3].x * size;
+		points[7] = this->segments[3].y * size;
+		points[8] = this->segments[5].x * size;
+		points[9] = this->segments[5].y * size;
+		points[10] = this->segments[4].x * size;
+		points[11] = this->segments[4].y * size;
+		obj->setOutline(6, points);
+		break;
+	}
+	default:
+		break;
+	}
 	return obj;
 }
--- AsteroidsOpenGL/fontBuilder.h
+++ AsteroidsOpenGL/fontBuilder.h
@@ -1,12 +1,48 @@
 #pragma once
 #include "gameObject.h"
 
+
 class fontBuilder
 {
 private:
-	fontBuilder() {}
+	
+	Point* segments;
+
+	fontBuilder() {
+
+		segments = nullptr;
+
+		Point lt, rt, lm, rm, bl, br;
+		lt.x = 0.01;
+		lt.y = -0.01;
+		rt.x = 0.3;
+		rt.y = -0.01;
+		lm.x = 0.01;
+		lm.x = -0.15;
+		rm.x = 0.3;
+		rm.y = -0.15;
+		bl.x = 0.01;
+		bl.y = -0.3;
+		br.x = 0.3;
+		br.y = -0.3;
+
+		segments = new Point[6];
+		segments[0] = lt;
+		segments[1] = rt;
+		segments[2] = lm;
+		segments[3] = rm;
+		segments[4] = bl;
+		segments[5] = br;
+	}
+
+	~fontBuilder() {
+		delete segments;
+	}
+	
+
 public:
-	gameObject* makeToText(Point p, float size);
+	gameObject* makeToText(int number, Point p, float size);
+
 	fontBuilder(fontBuilder const&) = delete;
 	void operator=(fontBuilder const&) = delete;
 	static fontBuilder& getInstance() {
