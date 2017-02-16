35fddf691c5f7571f5c9c87ec7f23ed7e22891b4
celphy <tobiascarstengoetz@gmail.com>
2017-02-16 23:35:49 +0100

    Added leftover numbers and added default playericon as output for non-existing numbers.

--- AsteroidsOpenGL/fontBuilder.cpp
+++ AsteroidsOpenGL/fontBuilder.cpp
@@ -102,7 +102,80 @@ gameObject * fontBuilder::makeToText(int number, Point p, float size)
 		obj->setOutline(6, points);
 		break;
 	}
+	case 6: {
+		GLfloat points[12];
+		points[0] = this->segments[1].x * size;
+		points[1] = this->segments[1].y * size;
+		points[2] = this->segments[0].x * size;
+		points[3] = this->segments[0].y * size;
+		points[4] = this->segments[4].x * size;
+		points[5] = this->segments[4].y * size;
+		points[6] = this->segments[5].x * size;
+		points[7] = this->segments[5].y * size;
+		points[8] = this->segments[3].x * size;
+		points[9] = this->segments[3].y * size;
+		points[10] = this->segments[2].x * size;
+		points[11] = this->segments[2].y * size;
+		obj->setOutline(6, points);
+	}
+	case 7: {
+		GLfloat points[6];
+		points[0] = this->segments[0].x * size;
+		points[1] = this->segments[0].y * size;
+		points[2] = this->segments[1].x * size;
+		points[3] = this->segments[1].y * size;
+		points[4] = this->segments[5].x * size;
+		points[5] = this->segments[5].y * size;
+		obj->setOutline(3, points);
+		break;
+	}
+
+	case 8: {
+		GLfloat points[14];
+		points[0] = this->segments[2].x * size;
+		points[1] = this->segments[2].y * size;
+		points[2] = this->segments[4].x * size;
+		points[3] = this->segments[4].y * size;
+		points[4] = this->segments[5].x * size;
+		points[5] = this->segments[5].x * size;
+		points[6] = this->segments[1].x * size;
+		points[7] = this->segments[1].y * size;
+		points[8] = this->segments[0].x * size;
+		points[9] = this->segments[0].y * size;
+		points[10] = this->segments[2].x * size;
+		points[11] = this->segments[2].y * size;
+		points[12] = this->segments[3].x * size;
+		points[13] = this->segments[3].y * size;
+		obj->setOutline(7, points);
+		break;
+	}
+
+	case 9: {
+		GLfloat points[10];
+		points[0] = this->segments[3].x * size;
+		points[1] = this->segments[3].y * size;
+		points[2] = this->segments[2].x * size,
+			points[3] = this->segments[2].y * size;
+		points[4] = this->segments[0].x * size;
+		points[5] = this->segments[0].y * size;
+		points[6] = this->segments[1].x * size;
+		points[7] = this->segments[1].y * size;
+		points[8] = this->segments[5].x * size;
+		points[9] = this->segments[5].y * size;
+		obj->setOutline(5, points);
+		break;
+	}
 	default:
+		GLfloat points[8];
+		points[0] = 0.15 * size;
+		points[1] = 0.01 * size;
+		points[2] = 0.3 * size;
+		points[3] = 0.3 * size;
+		points[4] = 0.01 * size;
+		points[5] = 0.3 * size;
+		points[6] = 0.15 * size;
+		points[7] = 0.01 * size;
+		obj->setOutline(4, points);
 		break;
 	}
 	return obj;
